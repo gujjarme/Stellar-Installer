038ae18928b5a253a87c01c86155ce817bc3a86e
Ahmad <a.bilal@xsquare.sa>
2024-10-15 19:44:07 +0500

    Update OSUtils.java

--- src/main/java/com/vaf/stellar/installationSteps/OSUtils.java
+++ src/main/java/com/vaf/stellar/installationSteps/OSUtils.java
@@ -7,21 +7,21 @@ public class OSUtils {
         if (os.contains("win")) {
             return "https://www.youtube.com/embed/9PKsZEDCLHc?&controls=1";
         } else{
-            return "https://www.youtube.com/embed/9PKsZEDCLHc?&controls=1";
+            return "https://www.youtube.com/embed/4TIMpzZwMOY?&controls=1";
         }
     }
     public static String getIntellijVideo(){
         if (os.contains("win")) {
             return "https://www.youtube.com/embed/JDiz99v13fo?&controls=1";
         } else{
-            return "https://www.youtube.com/embed/JDiz99v13fo?&controls=1";
+            return "https://www.youtube.com/embed/nAUJzOuzIf0?&controls=1";
         }
     }
     public static String getMavenURL() {
         if (os.contains("win")) {
             return "https://www.youtube.com/embed/g-ixjFtKPWk?&controls=1";
         } else{
-            return "https://www.youtube.com/embed/g-ixjFtKPWk?&controls=1";
+            return "https://www.youtube.com/embed/3Zh_aCY_GUw?&controls=1";
         }
     }
 
@@ -29,7 +29,7 @@ public class OSUtils {
         if (os.contains("win")) {
             return "https://www.youtube.com/embed/Ddb81dvT7Wc?1&controls=1";
         } else{
-            return "https://www.youtube.com/embed/Ddb81dvT7Wc?&controls=1";
+            return "https://www.youtube.com/embed/9glrISDStFQ?&controls=1";
         }
     }
     public static String getIntellijDocURL() {
